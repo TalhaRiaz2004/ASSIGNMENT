6bed857feb8d2b670cb40d5a24bf98d59541bc6b
TalhaRiaz2004 <talhabinriaz9@gmail.com>
2026-09-16 22:51:47 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+# Created by: TALHA BIN RIAZ
+from flask import Flask, jsonify
+
+app = Flask(__name_)
+
+
+@app.route("/")
+def home():
+    return "Hello from the DevOps Fundamentals sample app!"
+
+
+@app.route("/health")
+def health():
+    return jsonify(status="ok"), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
